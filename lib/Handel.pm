ff20149c4d40a3c91f7827e837a764f293d0b450
claco <claco@52a70e3a-55ec-0310-b14e-7917f69ecacc>
2004-12-30 05:03:56 +0000

    Added pod first draft
    git-svn-id: http://svn.icantfocus.com/CPAN/Handel/trunk@17 52a70e3a-55ec-0310-b14e-7917f69ecacc

--- lib/Handel.pm
+++ lib/Handel.pm
@@ -1,10 +1,9 @@
-# $Id$
 package Handel;
 use strict;
 use warnings;
 use vars qw($VERSION);
 
-$VERSION = '0.01_02';
+$VERSION = '0.01_03';
 
 1;
 __END__
@@ -13,9 +12,28 @@ __END__
 
 Handel - Simple commerce framework with AxKit support
 
+=head1 VERSION
+
+    $Id$
+
+=head1 DESCRIPTION
+
+Handel is a quick and not-so-dirty ecommerce framework with L<AxKit> taglib
+support. It was started for the conversion of an IIS/ASP based commerce site to
+Apache/ModPerl, but I decided that is might be usefull to others so here it is
+on CPAN.
+
+For the curious, Handel is German for commerce.
+
+=head1 SEE ALSO
+
+L<Handel::Cart>
+
 =head1 AUTHOR
 
     Christopher H. Laco
     CPAN ID: CLACO
     cpan@chrislaco.com
     http://today.icantfocus.com/blog/
+
+
