f7b365b8a1e09a213c996a1f316baa0126a1df06
claco <claco@52a70e3a-55ec-0310-b14e-7917f69ecacc>
2005-02-20 19:33:40 +0000

    Added T::P::Handel::Constants to access Handel::Constants
    git-svn-id: http://svn.icantfocus.com/CPAN/Handel/trunk@204 52a70e3a-55ec-0310-b14e-7917f69ecacc

---
+++ lib/Template/Plugin/Handel/Constants.pm
@@ -0,0 +1,56 @@
+package Template::Plugin::Handel::Constants;
+use strict;
+use warnings;
+use base 'Template::Plugin';
+use Handel::Constants ();
+
+sub new {
+    my ($class, $context, @params) = @_;
+    my $self = bless {_CONTEXT => $context}, ref($class) || $class;
+
+    foreach my $const (@Handel::Constants::EXPORT_OK) {
+        $self->{$const} = Handel::Constants->$const;
+    };
+
+    return $self;
+};
+
+sub load {
+    my ($class, $context) = @_;
+
+    return $class;
+};
+
+1;
+__END__
+
+=head1 NAME
+
+Template::Plugin::Handel::Constants - Template Toolkit plugin for Handel constants
+
+=head1 VERSION
+
+    $Id$
+
+=head1 SYNOPSIS
+
+    [% USE hdl = Handel.Constants %]
+    [% hdl.CART_TYPE_SAVED %]
+
+=head1 DESCRIPTION
+
+C<Template::Plugin::Handel::Constants> is a TT2 (Template Toolkit 2) plugin for
+Handel::Constants.
+
+It contains all of the exportable constants declared in @Handel::Constants::EXPORT_OK.
+
+=head1 SEE ALSO
+
+L<Handel::Constants>
+
+=head1 AUTHOR
+
+    Christopher H. Laco
+    CPAN ID: CLACO
+    cpan@chrislaco.com
+    http://today.icantfocus.com/blog/
