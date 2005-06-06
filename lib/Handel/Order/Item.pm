7d48bff24bf1ba4b40745e91cf9e4a200342e730
claco <claco@52a70e3a-55ec-0310-b14e-7917f69ecacc>
2005-06-06 00:07:51 +0000

    Added bulk of pod documentation Added various currency inflations to Handel::Currency
    git-svn-id: http://svn.icantfocus.com/CPAN/Handel/trunk@502 52a70e3a-55ec-0310-b14e-7917f69ecacc

--- lib/Handel/Order/Item.pm
+++ lib/Handel/Order/Item.pm
@@ -15,12 +15,38 @@ __PACKAGE__->autoupdate(0);
 __PACKAGE__->iterator_class('Handel::Iterator');
 __PACKAGE__->columns(All => qw(id order sku quantity price description total));
 __PACKAGE__->columns(Essential => qw(id order sku quantity price description total));
+__PACKAGE__->has_a(price => 'Handel::Currency');
+__PACKAGE__->has_a(total => 'Handel::Currency');
 __PACKAGE__->add_constraint('quantity', quantity => \&constraint_quantity);
 __PACKAGE__->add_constraint('price',    price    => \&constraint_price);
 __PACKAGE__->add_constraint('id',       id       => \&constraint_uuid);
-__PACKAGE__->add_constraint('cart',     cart     => \&constraint_uuid);
-__PACKAGE__->add_constraint('subtotal', subtotal => \&constraint_price);
+__PACKAGE__->add_constraint('order',    order    => \&constraint_uuid);
 __PACKAGE__->add_constraint('total',    total    => \&constraint_price);
+
 
 1;
 __END__
+
+=head1 NAME
+
+Handel::Order::Item - Module representing an indivudal order line item
+
+=head1 SYNOPSIS
+
+    my $order = Handel::Order->new({
+        id => '12345678-9098-7654-322-345678909876'
+    });
+
+    my $iterator = $order->items;
+    while (my $item = $iterator->next) {
+        print $item->sku;
+        print $item->price;
+        print $item->total;
+    };
+
+=head1 AUTHOR
+
+    Christopher H. Laco
+    CPAN ID: CLACO
+    claco@chrislaco.com
+    http://today.icantfocus.com/blog/
