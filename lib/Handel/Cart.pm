f639a17aeb0d369afa788d8a47d2f23f15157490
claco <claco@52a70e3a-55ec-0310-b14e-7917f69ecacc>
2005-06-09 01:41:51 +0000

    Changed item object copy to use columns instead of internal hash.
    git-svn-id: http://svn.icantfocus.com/CPAN/Handel/trunk@519 52a70e3a-55ec-0310-b14e-7917f69ecacc

--- lib/Handel/Cart.pm
+++ lib/Handel/Cart.pm
@@ -53,7 +53,12 @@ sub add {
 
         return $self->add_to__items($data);
     } else {
-        my %copy = %{$data};
+        my %copy;
+
+        foreach ($data->columns) {
+            next if $_ =~ /^(id|cart)$/i;
+            $copy{$_} = $data->$_;
+        };
 
         $copy{'id'} = $self->uuid;
 
@@ -365,7 +370,7 @@ containing all items.
 
     my $iterator = $cart->items;
     while (my $item = $iterator->next) {
-        print $cart->sku;
+        print $item->sku;
     };
 
     my @items = $cart->items;
