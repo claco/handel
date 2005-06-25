fb936815ee9ac6c6b8c7a17169db3895e7e987d2
claco <claco@52a70e3a-55ec-0310-b14e-7917f69ecacc>
2005-06-25 02:18:04 +0000

    Added test plugins
    git-svn-id: http://svn.icantfocus.com/CPAN/Handel/trunk@551 52a70e3a-55ec-0310-b14e-7917f69ecacc

---
+++ t/lib/Handel/Checkout/Plugin/TestPlugin.pm
@@ -0,0 +1,39 @@
+# $Id$
+package Handel::Checkout::Plugin::TestPlugin;
+use strict;
+use warnings;
+use base 'Handel::Checkout::Plugin';
+use Handel::Constants qw(:checkout);
+
+sub register {
+    my ($self, $ctx) = @_;
+
+    $self->{'register_called'}++;
+    $ctx->add_handler(CHECKOUT_PHASE_INITIALIZE, \&handler);
+};
+
+sub init {
+    my ($self, $ctx) = @_;
+
+    $self->{'init_called'}++;
+};
+
+sub setup {
+    my ($self, $ctx) = @_;
+
+    $self->{'setup_called'}++;
+};
+
+sub teardown {
+    my ($self, $ctx) = @_;
+
+    $self->{'teardown_called'}++;
+};
+
+sub handler {
+    my ($self, $ctx) = @_;
+
+    $self->{'handler_called'}++;
+};
+
+1;
