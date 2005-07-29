da2927b08f48d5410856a0f105579305fa0a1ef1
claco <claco@52a70e3a-55ec-0310-b14e-7917f69ecacc>
2005-07-29 01:40:42 +0000

    Added Checkout Taglib shell code
    git-svn-id: http://svn.icantfocus.com/CPAN/Handel/trunk@595 52a70e3a-55ec-0310-b14e-7917f69ecacc

---
+++ lib/AxKit/XSP/Handel/Checkout.pm
@@ -0,0 +1,51 @@
+# $Id$
+package AxKit::XSP::Handel::Checkout;
+use strict;
+use warnings;
+use vars qw($NS);
+use Handel::ConfigReader;
+use Handel::Constants qw(:checkout str_to_const);
+use Handel::Exception;
+use Handel::L10N qw(translate);
+use base 'Apache::AxKit::Language::XSP';
+
+$NS  = 'http://today.icantfocus.com/CPAN/AxKit/XSP/Handel/Checkout';
+
+{
+ sub parse_char {
+
+ };
+};
+
+1;
+__END__
+
+=head1 NAME
+
+AxKit::XSP::Handel::Checkout - AxKit XSP Checkout Taglib
+
+=head1 SYNOPSIS
+
+Add this taglib to AxKit in your http.conf or .htaccess:
+
+    AxAddXSPTaglib AxKit::XSP::Handel::Checkout
+
+Add the namespace to your XSP file and use the tags:
+
+    <xsp:page
+         language="Perl"
+         xmlns:xsp="http://apache.org/xsp/core/v1"
+         xmlns:order="http://today.icantfocus.com/CPAN/AxKit/XSP/Handel/Checkout"
+    >
+
+=head1 DESCRIPTION
+
+This tag library provides an interface to use C<Handel::Checkout> inside of your
+AxKit XSP pages.
+
+=head1 AUTHOR
+
+    Christopher H. Laco
+    CPAN ID: CLACO
+    claco@chrislaco.com
+    http://today.icantfocus.com/blog/
