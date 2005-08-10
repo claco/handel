0d594e658c882179e0c01c00ebf2ec7a03c477c5
claco <claco@52a70e3a-55ec-0310-b14e-7917f69ecacc>
2005-08-10 01:25:49 +0000

    Added test pipeline
    git-svn-id: http://svn.icantfocus.com/CPAN/Handel/trunk@693 52a70e3a-55ec-0310-b14e-7917f69ecacc

---
+++ t/lib/Handel/TestPipeline/InitializeTotals.pm
@@ -0,0 +1,43 @@
+# $Id$
+package Handel::TestPipeline::InitializeTotals;
+use strict;
+use warnings;
+use base 'Handel::Checkout::Plugin';
+use Handel::Constants qw(:checkout :returnas);
+
+sub register {
+    my ($self, $ctx) = @_;
+
+    $ctx->add_handler(CHECKOUT_PHASE_INITIALIZE, \&handler);
+};
+
+sub handler {
+    my ($self, $ctx) = @_;
+
+    if (my $order = $ctx->order) {
+
+        my $subtotal = 0;
+
+        eval {
+            my $items = $order->items(undef, RETURNAS_ITERATOR);
+            while (my $item = $items->next) {
+                $item->total($item->quantity*$item->price);
+                $subtotal += $item->total;
+            };
+            $order->subtotal($subtotal);
+        };
+
+        if ($@) {
+            $ctx->add_message($@);
+            return CHECKOUT_HANDLER_ERROR;
+        } else {
+            return CHECKOUT_HANDLER_OK;
+        };
+    } else {
+        $ctx->add_message('No order was loaded');
+
+        return CHECKOUT_HANDLER_ERROR;
+    };
+};
+
+1;
---
+++ t/lib/Handel/TestPipeline/ValidateError.pm
@@ -0,0 +1,45 @@
+# $Id$
+package Handel::TestPipeline::ValidateError;
+use strict;
+use warnings;
+use base 'Handel::Checkout::Plugin';
+use Handel::Constants qw(:checkout :returnas);
+
+sub register {
+    my ($self, $ctx) = @_;
+
+    $ctx->add_handler(CHECKOUT_PHASE_VALIDATE, \&handler);
+};
+
+sub handler {
+    my ($self, $ctx) = @_;
+
+    if (my $order = $ctx->order) {
+
+        my $subtotal = 0;
+
+        eval {
+            my $items = $order->items(undef, RETURNAS_ITERATOR);
+            while (my $item = $items->next) {
+                $item->sku('ERRORSKU');
+            };
+            $order->billtofirstname('ErrorBillToFirstName');
+            $order->billtolastname('ErrorBillToLastName');
+
+            die 'ValidateError';
+        };
+
+        if ($@) {
+            $ctx->add_message($@);
+            return CHECKOUT_HANDLER_ERROR;
+        } else {
+            return CHECKOUT_HANDLER_OK;
+        };
+    } else {
+        $ctx->add_message('No order was loaded');
+
+        return CHECKOUT_HANDLER_ERROR;
+    };
+};
+
+1;
