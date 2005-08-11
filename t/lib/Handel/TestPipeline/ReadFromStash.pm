ae9447f070b75aa251e97184775ccfc5e4bff624
claco <claco@52a70e3a-55ec-0310-b14e-7917f69ecacc>
2005-08-11 01:25:20 +0000

    Added stash reader/writer
    git-svn-id: http://svn.icantfocus.com/CPAN/Handel/trunk@709 52a70e3a-55ec-0310-b14e-7917f69ecacc

---
+++ t/lib/Handel/TestPipeline/ReadFromStash.pm
@@ -0,0 +1,22 @@
+# $Id$
+package Handel::TestPipeline::ReadFromStash;
+use strict;
+use warnings;
+use base 'Handel::Checkout::Plugin';
+use Handel::Constants qw(:checkout :returnas);
+
+sub register {
+    my ($self, $ctx) = @_;
+
+    $ctx->add_handler(CHECKOUT_PHASE_DELIVER, \&handler);
+};
+
+sub handler {
+    my ($self, $ctx) = @_;
+
+    $self->{'ReadFromStash'} = $ctx->stash->{'WriteToStash'};
+
+    return CHECKOUT_HANDLER_OK;
+};
+
+1;
---
+++ t/lib/Handel/TestPipeline/WriteToStash.pm
@@ -0,0 +1,22 @@
+# $Id$
+package Handel::TestPipeline::WriteToStash;
+use strict;
+use warnings;
+use base 'Handel::Checkout::Plugin';
+use Handel::Constants qw(:checkout :returnas);
+
+sub register {
+    my ($self, $ctx) = @_;
+
+    $ctx->add_handler(CHECKOUT_PHASE_INITIALIZE, \&handler);
+};
+
+sub handler {
+    my ($self, $ctx) = @_;
+
+    $ctx->stash->{'WriteToStash'} = 'WrittenToStash';
+
+    return CHECKOUT_HANDLER_OK;
+};
+
+1;
