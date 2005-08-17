f5c7c07cfa1e20d569c4836e757550471f791219
claco <claco@52a70e3a-55ec-0310-b14e-7917f69ecacc>
2005-08-17 02:07:27 +0000

    Added new, messages, message, plugins, and plugin tags.
    git-svn-id: http://svn.icantfocus.com/CPAN/Handel/trunk@722 52a70e3a-55ec-0310-b14e-7917f69ecacc

--- lib/AxKit/XSP/Handel/Checkout.pm
+++ lib/AxKit/XSP/Handel/Checkout.pm
@@ -12,9 +12,149 @@ use base 'Apache::AxKit::Language::XSP';
 $NS  = 'http://today.icantfocus.com/CPAN/AxKit/XSP/Handel/Checkout';
 
 {
- sub parse_char {
+    my @context = 'root';
 
- };
+    sub start_document {
+        return "use Handel::Checkout;\n";
+    };
+
+    sub parse_char {
+        my ($e, $text) = @_;
+        my $tag = $e->current_element();
+
+        return unless length($text);
+
+        if ($tag eq 'addmessage') {
+            return "\n\$_xsp_handel_checkout_addmessage_hash{'text'} = q|" . $text . "|;\n";
+        };
+
+        return '';
+    };
+
+    sub parse_start {
+        my ($e, $tag, %attr) = @_;
+
+        AxKit::Debug(5, "[Handel] [Checkout] parse_start [$tag] context: " . join('->', @context));
+
+        if ($tag eq 'new') {
+            throw Handel::Exception::Taglib(
+                -text => translate("Tag '[_1]' not valid inside of other Handel tags", $tag)
+            ) if ($context[$#context] ne 'root');
+
+            push @context, $tag;
+
+            my $code = "my \$_xsp_handel_checkout;my \$_xsp_handel_checkout_new_options;my \$_xsp_handel_checkout_new_called;\n";
+            $code .= scalar keys %attr ?
+                'my %_xsp_handel_checkout_new_filter = ("' . join('", "', %attr) . '");' :
+                'my %_xsp_handel_checkout_new_filter;' ;
+
+            return "\n{\n$code\n";
+        } elsif ($tag eq 'plugins') {
+            push @context, $tag;
+
+            my $code = '
+                if (!$_xsp_handel_checkout_new_called) {
+                    $_xsp_handel_checkout = Handel::Checkout->new(\%_xsp_handel_checkout_new_filter);
+                    $_xsp_handel_checkout_new_called++;
+                };
+                foreach my $_xsp_handel_checkout_plugin ($_xsp_handel_checkout->plugins) {
+            ';
+
+            return $code;
+        } elsif ($tag eq 'plugin') {
+            throw Handel::Exception::Taglib(
+                -text => translate("Tag '[_1]' not valid inside of tag '" . $context[$#context] . "'", $tag)
+            ) if ($context[$#context] ne 'plugins');
+
+            push @context, $tag;
+
+            $e->start_expr($tag);
+            $e->append_to_script("ref \$_xsp_handel_checkout_plugin;\n");
+            $e->end_expr($tag);
+        } elsif ($tag eq 'messages') {
+            push @context, $tag;
+
+            my $code = '
+                if (!$_xsp_handel_checkout_new_called) {
+                    $_xsp_handel_checkout = Handel::Checkout->new(\%_xsp_handel_checkout_new_filter);
+                    $_xsp_handel_checkout_new_called++;
+                };
+                foreach my $_xsp_handel_checkout_message ($_xsp_handel_checkout->messages) {
+            ';
+
+            return $code;
+        } elsif ($tag eq 'message' || $tag =~ /^message\-(.*)$/) {
+            my $property = $1;
+
+            # I have no friggin clue how the regexp above on this tag can
+            # yield checkout, but it does. :-)
+            undef $property if ($property eq 'checkout');
+
+            throw Handel::Exception::Taglib(
+                -text => translate("Tag '[_1]' not valid inside of tag '" . $context[$#context] . "'", $tag)
+            ) if ($context[$#context] ne 'messages');
+
+            push @context, $tag;
+
+            $e->start_expr($tag);
+            if ($property) {
+                $e->append_to_script("\$_xsp_handel_checkout_message->$property;\n");
+            } else {
+                $e->append_to_script("\$_xsp_handel_checkout_message;\n");
+            };
+            $e->end_expr($tag);
+        } elsif ($tag eq 'addmessage') {
+            my $code = 'my $_xsp_handel_checkout_addmessage_hash;
+                if (!$_xsp_handel_checkout_new_called) {
+                    $_xsp_handel_checkout = Handel::Checkout->new(\%_xsp_handel_checkout_new_filter);
+                    $_xsp_handel_checkout_new_called++;
+                };
+            ';
+
+            $code .= scalar keys %attr ?
+                'my %_xsp_handel_checkout_addmessage_hash = ("' . join('", "', %attr) . '");' :
+                'my %_xsp_handel_checkout_addmessage_hash;' ;
+
+            return "\n{\n$code\n";
+        };
+
+        return '';
+    };
+
+    sub parse_end {
+        my ($e, $tag) = @_;
+
+        AxKit::Debug(5, "[Handel] [Checkout] parse_end   [$tag] context: " . join('->', @context));
+
+        if ($tag eq 'new') {
+            pop @context;
+
+            return "\n};\n";
+        } elsif ($tag eq 'plugins') {
+            pop @context;
+
+            return '
+                };
+            ';
+        } elsif ($tag eq 'plugin') {
+            pop @context;
+        } elsif ($tag eq 'messages') {
+            pop @context;
+
+            return '
+                };
+            ';
+        } elsif ($tag eq 'message' || $tag =~ /^message-(.*)$/) {
+            pop @context;
+        } elsif ($tag eq 'addmessage') {
+            return '
+                    $_xsp_handel_checkout->add_message(Handel::Checkout::Message->new(%_xsp_handel_checkout_addmessage_hash));
+                };
+            ';
+        };
+
+        return '';
+    };
 };
 
 1;
@@ -42,6 +182,12 @@ Add the namespace to your XSP file and use the tags:
 
 This tag library provides an interface to use C<Handel::Checkout> inside of your
 AxKit XSP pages.
+
+=head1 TAG HIERARCHY
+
+    <checkout:new pluginpaths|addpluginpaths|loadplugins|ignoreplugins|order|cart|="value"...>
+
+    </checkout:new>
 
 =head1 AUTHOR
 
