9e1a476ed4c836b1d69b02b88e2aba6bd56df867
claco <claco@52a70e3a-55ec-0310-b14e-7917f69ecacc>
2005-09-17 23:31:32 +0000

    Inlined Data::FormValidator::Results::msgs with patch until the next DFV release
    git-svn-id: http://svn.icantfocus.com/CPAN/Handel/trunk@826 52a70e3a-55ec-0310-b14e-7917f69ecacc

--- lib/Catalyst/Helper/Controller/Handel/Checkout.pm
+++ lib/Catalyst/Helper/Controller/Handel/Checkout.pm
@@ -52,9 +52,8 @@ use base 'Catalyst::Base';
 our $DFV;
 our $FIF;
 
-# Until this patch [hopefully] get's dumped into DFV 4.03, apply it and
-# update FormValidator.pm verison to 4.03 to make this controller work with
-# Data::FormValidator
+# Until this patch [hopefully] get's dumped into DFV 4.03, I've inlined the msgs
+# method below with the following path applied to it:
 #
 #--- Results.pm.orig Wed Aug 31 22:27:27 2005
 #+++ Results.pm  Wed Sep 14 17:40:28 2005
@@ -75,8 +74,79 @@ BEGIN {
         $FIF = HTML::FillInForm->new;
     };
 
-    eval 'use Data::FormValidator 4.03';
+    eval 'use Data::FormValidator';
     if (!$@) {
+        #############################################################
+        # This is here until the patch makes it to release
+        #############################################################
+        no warnings 'redefine';
+        sub Data::FormValidator::Results::msgs {
+            my $self = shift;
+            my $controls = shift || {};
+            if (defined $controls and ref $controls ne 'HASH') {
+                die "$0: parameter passed to msgs must be a hash ref";
+            }
+
+
+            # Allow msgs to be called more than one to accumulate error messages
+            $self->{msgs} ||= {};
+            $self->{profile}{msgs} ||= {};
+            $self->{msgs} = { %{ $self->{msgs} }, %$controls };
+
+            # Legacy typo support.
+            for my $href ($self->{msgs}, $self->{profile}{msgs}) {
+                if (
+                     (not defined $href->{invalid_separator})
+                     &&  (defined $href->{invalid_seperator})
+                 ) {
+                    $href->{invalid_separator} = $href->{invalid_seperator};
+                }
+            }
+
+            my %profile = (
+                prefix  => '',
+                missing => 'Missing',
+                invalid => 'Invalid',
+                invalid_separator => ' ',
+
+                format  => '<span style="color:red;font-weight:bold"><span class="dfv_errors">* %s</span></span>',
+                %{ $self->{msgs} },
+                %{ $self->{profile}{msgs} },
+            );
+
+
+            my %msgs = ();
+
+            # Add invalid messages to hash
+                #  look at all the constraints, look up their messages (or provide a default)
+                #  add field + formatted constraint message to hash
+            if ($self->has_invalid) {
+                my $invalid = $self->invalid;
+                for my $i ( keys %$invalid ) {
+                    $msgs{$i} = join $profile{invalid_separator}, map {
+                        Data::FormValidator::Results::_error_msg_fmt($profile{format},($profile{constraints}{$_} || $profile{invalid}))
+                        } @{ $invalid->{$i} };
+                }
+            }
+
+            # Add missing messages, if any
+            if ($self->has_missing) {
+                my $missing = $self->missing;
+                for my $m (@$missing) {
+                    $msgs{$m} = Data::FormValidator::Results::_error_msg_fmt($profile{format},
+                      (ref $profile{missing} eq 'HASH' ?
+                          ($profile{missing}->{$m} || $profile{missing}->{default} || 'Missing') : $profile{missing}));
+                }
+            }
+
+            my $msgs_ref = Data::FormValidator::Results::prefix_hash($profile{prefix},\%msgs);
+
+            $msgs_ref->{ $profile{any_errors} } = 1 if defined $profile{any_errors};
+
+            return $msgs_ref;
+        }
+        #############################################################
+
         $DFV = Data::FormValidator->new({
             checkout_update => {
                 required => [qw/billtofirstname
--- lib/Catalyst/Helper/Controller/Handel/Order.pm
+++ lib/Catalyst/Helper/Controller/Handel/Order.pm
@@ -37,9 +37,8 @@ use base 'Catalyst::Base';
 
 our $DFV;
 
-# Until this patch [hopefully] get's dumped into DFV 4.03, apply it and
-# update FormValidator.pm verison to 4.03 to make this controller work with
-# Data::FormValidator
+# Until this patch [hopefully] get's dumped into DFV 4.03, I've inlined the msgs
+# method below with the following path applied to it:
 #
 #--- Results.pm.orig Wed Aug 31 22:27:27 2005
 #+++ Results.pm  Wed Sep 14 17:40:28 2005
@@ -55,8 +54,79 @@ our $DFV;
 #    }
 
 BEGIN {
-    eval 'use Data::FormValidator 4.03';
+    eval 'use Data::FormValidator';
     if (!$@) {
+        #############################################################
+        # This is here until the patch makes it to release
+        #############################################################
+        no warnings 'redefine';
+        sub Data::FormValidator::Results::msgs {
+            my $self = shift;
+            my $controls = shift || {};
+            if (defined $controls and ref $controls ne 'HASH') {
+                die "$0: parameter passed to msgs must be a hash ref";
+            }
+
+
+            # Allow msgs to be called more than one to accumulate error messages
+            $self->{msgs} ||= {};
+            $self->{profile}{msgs} ||= {};
+            $self->{msgs} = { %{ $self->{msgs} }, %$controls };
+
+            # Legacy typo support.
+            for my $href ($self->{msgs}, $self->{profile}{msgs}) {
+                if (
+                     (not defined $href->{invalid_separator})
+                     &&  (defined $href->{invalid_seperator})
+                 ) {
+                    $href->{invalid_separator} = $href->{invalid_seperator};
+                }
+            }
+
+            my %profile = (
+                prefix  => '',
+                missing => 'Missing',
+                invalid => 'Invalid',
+                invalid_separator => ' ',
+
+                format  => '<span style="color:red;font-weight:bold"><span class="dfv_errors">* %s</span></span>',
+                %{ $self->{msgs} },
+                %{ $self->{profile}{msgs} },
+            );
+
+
+            my %msgs = ();
+
+            # Add invalid messages to hash
+                #  look at all the constraints, look up their messages (or provide a default)
+                #  add field + formatted constraint message to hash
+            if ($self->has_invalid) {
+                my $invalid = $self->invalid;
+                for my $i ( keys %$invalid ) {
+                    $msgs{$i} = join $profile{invalid_separator}, map {
+                        Data::FormValidator::Results::_error_msg_fmt($profile{format},($profile{constraints}{$_} || $profile{invalid}))
+                        } @{ $invalid->{$i} };
+                }
+            }
+
+            # Add missing messages, if any
+            if ($self->has_missing) {
+                my $missing = $self->missing;
+                for my $m (@$missing) {
+                    $msgs{$m} = Data::FormValidator::Results::_error_msg_fmt($profile{format},
+                      (ref $profile{missing} eq 'HASH' ?
+                          ($profile{missing}->{$m} || $profile{missing}->{default} || 'Missing') : $profile{missing}));
+                }
+            }
+
+            my $msgs_ref = Data::FormValidator::Results::prefix_hash($profile{prefix},\%msgs);
+
+            $msgs_ref->{ $profile{any_errors} } = 1 if defined $profile{any_errors};
+
+            return $msgs_ref;
+        }
+        #############################################################
+
         $DFV = Data::FormValidator->new({
             orders_view    => {
                 required => [qw/id/],
