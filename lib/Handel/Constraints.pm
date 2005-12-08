8c946a00cec0ba1a35beadd8ba03b5a94ff83dde
claco <claco@52a70e3a-55ec-0310-b14e-7917f69ecacc>
2005-12-08 04:51:01 +0000

    constraint_checkout_phase now inspects CHECKOUT_ALL_PHASES
    git-svn-id: http://svn.icantfocus.com/CPAN/Handel/trunk@1005 52a70e3a-55ec-0310-b14e-7917f69ecacc

--- lib/Handel/Constraints.pm
+++ lib/Handel/Constraints.pm
@@ -95,12 +95,13 @@ sub constraint_checkout_phase {
 
     return if $value !~ /[0-9]/;
 
-    if ($value != CHECKOUT_PHASE_INITIALIZE && $value != CHECKOUT_PHASE_VALIDATE &&
-        $value != CHECKOUT_PHASE_FINALIZE && $value != CHECKOUT_PHASE_AUTHORIZE &&
-        $value != CHECKOUT_PHASE_DELIVER) {
-        return 0;
+    foreach my $const (@{Handel::Constants->CHECKOUT_ALL_PHASES}) {
+        if ($value == $const) {
+            return 1;
+        };
     };
-    return 1;
+
+    return 0;
 };
 
 sub constraint_order_type {
