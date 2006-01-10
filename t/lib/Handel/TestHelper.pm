dac9ef0f708dc26fd50c0667efa5764c97d42a63
claco <claco@52a70e3a-55ec-0310-b14e-7917f69ecacc>
2006-01-10 00:06:26 +0000

    Added PRAGMA setting to east disk thrashing during tests
    git-svn-id: http://svn.icantfocus.com/CPAN/Handel/trunk@1064 52a70e3a-55ec-0310-b14e-7917f69ecacc

--- t/lib/Handel/TestHelper.pm
+++ t/lib/Handel/TestHelper.pm
@@ -15,6 +15,10 @@ sub executesql {
     return unless ($db || $sqlfile);
 
     my $dbh = DBI->connect($db);
+
+    $dbh->do('PRAGMA synchronous = OFF');
+    $dbh->do('PRAGMA temp_store = MEMORY');
+
     open SQL, "< $sqlfile";
     while (<SQL>) {
         $dbh->do($_);
@@ -46,6 +50,9 @@ sub preparetables {
     my $dbh = DBI->connect($db);
     foreach my $group (@{$groups}) {
         return unless exists $tables{$group};
+
+        $dbh->do('PRAGMA synchronous = OFF');
+        $dbh->do('PRAGMA temp_store = MEMORY');
 
         my $data = $tables{$group};
         my ($count) = $dbh->selectrow_array("SELECT COUNT(*) FROM sqlite_master WHERE type='table' AND name='" . $data->{'exists'} . "'");
